ba174234b24ac10c4e9db5b049fa857aec392c03
BT_1N3_07 <brandonjl@nycstudents.net>
2025-01-15 09:52:28 -0500

    Completed Th-10

--- src/TreasureHunter.java
+++ src/TreasureHunter.java
@@ -64,6 +64,7 @@ public class TreasureHunter {
             hunter.addItem("machete");
             hunter.addItem("horse");
             hunter.addItem("boat");
+            hunter.addItem("shovel");
         }
         if (hard.equals("e")) {
             easyMode=true;
@@ -75,18 +76,18 @@ public class TreasureHunter {
      * Creates a new town and adds the Hunter to it.
      */
     private void enterTown() {
-        double markdown = 0.25;
+        double markdown = 0.5;
         double toughness = 0.4;
         if (hardMode) {
             // in hard mode, you get less money back when you sell items
-            markdown = 0.5;
+            markdown = 0.25;
 
             // and the town is "tougher"
             toughness = 0.75;
         }
         if (easyMode) {
             hunter.changeGold(20);
-            markdown=0;
+            markdown=1;
         }
 
         // note that we don't need to access the Shop object
